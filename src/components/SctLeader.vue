582fb08635c4b5145f07aeaaf217a1114ba8d7ef
AntoCic <anto.cic.127@gmail.com>
2024-04-23 16:49:17 +0200

    ADDED leader section

--- src/components/SctLeader.vue
+++ src/components/SctLeader.vue
@@ -1,10 +1,39 @@
 <template>
-    <section class="  w-100">
-        
+    <section class="leader w-100 p-sct">
+        <div class="ms_container-md">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <h2 class="sct-title">Creative Leader</h2>
+                        <div class="sct-sub-title sct-sub-line"></div>
+                        <p class="sct-sub-title"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
+                            ipsam modi
+                            dolorem at magni tempora similique repudiandae sed harum! Autem iusto sunt adipisci,
+                            repellendus
+                            voluptatibus perferendis nisi? Repudiandae voluptates ut consequuntur aliquam et repellendus
+                            veniam
+                            delectus, pariatur sapiente! Saepe hic illo libero sequi explicabo eos possimus totam
+                            aspernatur quo
+                            sapiente.</p>
+                            <p>READ MORE</p>
+                    </div>
+                    <div class="col">
+                        <div class="d-flex justify-content-between ">
+                            <h4>Mentorship</h4> 
+                            <span>78%</span>
+                        </div>
+                        <div class="d-flex justify-content-between ">
+                            <h4>Mentorship</h4> 
+                            <span>78%</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
     </section>
 
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
