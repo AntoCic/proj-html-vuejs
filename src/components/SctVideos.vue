02f3ede72e7be069e1ba418e761ff055cc41dd2e
AntoCic <anto.cic.127@gmail.com>
2024-04-23 18:14:26 +0200

    ADDED video section

--- src/components/SctVideos.vue
+++ src/components/SctVideos.vue
@@ -1,10 +1,12 @@
 <template>
-    <section class="  w-100">
-        
+    <section class="videos w-100 position-relative">
+        <img src="../assets/img/h1-img-04.jpg" class="w-100">
+
+        <div class="position-absolute top-50 start-50 translate-middle">
+            <i class="bi bi-play-fill fs-1 text-white" role="button"></i>
+        </div>
     </section>
 
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
