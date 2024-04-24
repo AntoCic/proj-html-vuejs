f931b88d7aa447fcffa098f866c49a4e345bcb79
AntoCic <anto.cic.127@gmail.com>
2024-04-24 17:46:55 +0200

    FIXED testimonial

--- src/components/SctTestimonials.vue
+++ src/components/SctTestimonials.vue
@@ -2,6 +2,8 @@
     <section class="testimonial w-100 ms_bg-black text-white text-center">
         <div class="ms_container-lg p-sct position-relative overflow-hidden">
             <p class="bg-text">Testimonials.</p>
+            <ElmArrow class="arr-left position-absolute top-50 start-0 translate-middle-y" color="#fff" />
+            <ElmArrow class="arr-right position-absolute top-50 end-0 translate-middle-y" color="#fff" />
             <div class="user position-relative">
                 <div class="user-img">
                     <img src="../assets/img/h3-img-04.png">
@@ -11,6 +13,7 @@
                     <p class="text-gray">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto repellat
                         animi odit minus porro suscipit inventore minima praesentium recusandae non.</p>
                 </div>
+                <div class="position-indicators text-gray">01 <div class="carosel-bar"></div> 03</div>
 
             </div>
         </div>
@@ -18,6 +21,13 @@
     </section>
 
 </template>
+
+<script>
+import ElmArrow from './ElmArrow.vue';
+export default {
+    components: { ElmArrow },
+}
+</script>
 
 <style lang="scss" scoped>
 @use '../assets/scss/partials/_variables.scss' as *;
@@ -33,6 +43,7 @@
     right: 0;
     left: 0;
     color: black;
+    user-select: none;
 }
 
 .user {
@@ -59,8 +70,28 @@
             border-radius: 50%;
         }
     }
+}
 
+.carosel-bar {
+    display: inline-block;
+    height: 1px;
+    width: 120px;
+    background-color: $gray;
+    vertical-align: middle;
+    margin-right: 8px;
+    margin-left: 8px;
+    position: relative;
 
-
+    &::after {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        height: 1px;
+        width: 33%;
+        background-color: white;
+    }
 }
 </style>
