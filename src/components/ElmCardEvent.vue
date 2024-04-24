a7d8d0e14fffee86e45b2b9fdd3ae69b0be3a864
AntoCic <anto.cic.127@gmail.com>
2024-04-24 15:02:15 +0200

    FIXED event section

---
+++ src/components/ElmCardEvent.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="list-items">
+    <div class="lc-data">
+      <p class="mb-0 fw-bolder">{{ eventObj.day }}</p>
+      <p class="mb-0 fw-lighter">{{ eventObj.data }}</p>
+    </div>
+    <div class="lc-contents">
+      <h3 class="lc-title">{{ eventObj.title }}</h3>
+      <p class="lc-info"><i class="bi bi-clock"></i>{{ eventObj.dataTime }}</p>
+      <p class="lc-info"><i class="bi bi-geo-alt"></i>{{ eventObj.location }}</p>
+
+      <a href="#" class="read-more"></a>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['eventObj'],
+  methods: {
+    imgSocialPath(fileName) {
+      return `/src/assets/img/social/${fileName}`
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@use '../assets/scss/partials/_variables.scss' as *;
+
+.list-items {
+  display: flex;
+  padding: 30px 44px;
+  border-top: 2px solid #EDEDED;
+
+  &:nth-child(2) {
+    background-color: #fff;
+  }
+
+
+  .lc-data {
+    width: 70px;
+    height: 55px;
+    background-color: $orange;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    color: #fff;
+    padding: 10px 10px;
+    font-size: 12px;
+
+    p:first-child {
+      font-size: 18px;
+    }
+  }
+
+  .lc-contents {
+    width: 80%;
+    padding: 0 20px;
+    font-family: "Libre Baskerville", serif;
+    font-weight: 400;
+    font-style: normal;
+
+    .lc-title {
+      font-size: 20px;
+      font-weight: 700;
+
+      &:hover {
+        color: $orange;
+      }
+    }
+
+    .lc-info {
+      color: $gray;
+
+      i {
+        margin-right: 12px;
+        vertical-align: text-bottom;
+        color: $orange;
+      }
+    }
+  }
+
+}
+</style>
--- src/components/SctEvents.vue
+++ src/components/SctEvents.vue
@@ -3,48 +3,11 @@
         <img class="w-100" src="/src/assets/img/h1-img-09.jpg ">
         <div class="ms_container-md d-flex align-items-center">
             <div class="box-list">
-                <div class="list-items">
-                    <h2 class="sct-title no-dot">Upcoming Events</h2>
-                </div>
-                <div class="list-items">
-                    <div class="lc-data">
-                        <p class="mb-0">07</p>
-                        <p class="mb-0">Jan,2022</p>
-                    </div>
-                    <div class="lc-contents">
-                        <h3 class="lc-title">Melbourne Coaching</h3>
-                        <p class="lc-info">9:00 am - 5:00 pm, Jan 11,2022</p>
-                    <p class="lc-info">Cambridge, MA D2138, USA</p>
-                    <a href="#" class="lc-read">READ MORE</a>
-                    </div>
-                    
-                </div>
-                <div class="list-items">
-                    <div class="lc-data">
-                        <p class="mb-0">07</p>
-                        <p class="mb-0">Jan,2022</p>
-                    </div>
-                    <div class="lc-contents">
-                        <h3 class="lc-title">Melbourne Coaching</h3>
-                        <p class="lc-info">9:00 am - 5:00 pm, Jan 11,2022</p>
-                    <p class="lc-info">Cambridge, MA D2138, USA</p>
-                    <a href="#" class="lc-read">READ MORE</a>
-                    </div>
-                    
-                </div>
-                <div class="list-items">
-                    <div class="lc-data">
-                        <p class="mb-0">07</p>
-                        <p class="mb-0">Jan,2022</p>
-                    </div>
-                    <div class="lc-contents">
-                        <h3 class="lc-title">Melbourne Coaching</h3>
-                        <p class="lc-info">9:00 am - 5:00 pm, Jan 11,2022</p>
-                    <p class="lc-info">Cambridge, MA D2138, USA</p>
-                    <a href="#" class="lc-read">READ MORE</a>
-                    </div>
-                    
-                </div>
+                <h2 class="sct-title no-dot">Upcoming Events</h2>
+
+                <ElmCardEvent :eventObj="event1"/>
+                <ElmCardEvent :eventObj="event2"/>
+                <ElmCardEvent :eventObj="event3"/>
 
             </div>
         </div>
@@ -52,9 +15,39 @@
 
 </template>
 
-<style lang="scss" scoped>
-@use '../assets/scss/partials/_variables.scss' as *;
+<script>
+import ElmCardEvent from './ElmCardEvent.vue'
+export default {
+    components: { ElmCardEvent },
+    data() {
+        return {
+            event1: {
+                day: '07',
+                data: 'Jan,2022',
+                dataTime: '9:00 am - 5:00 pm, Jan 11,2022',
+                title: 'Melbourne Coaching',
+                location: 'Cambridge, MA D2138, USA'
+            },
+            event2: {
+                day: '11',
+                data: 'Jan,2022',
+                dataTime: '9:00 am - 5:00 pm, Jan 11,2022',
+                title: 'New York Coaching',
+                location: 'Cambridge, MA D2138, USA'
+            },
+            event3: {
+                day: '21',
+                data: 'Jan,2022',
+                dataTime: '9:00 am - 5:00 pm, Jan 11,2022',
+                title: 'London Coaching',
+                location: 'Cambridge, MA D2138, USA'
+            }
+        }
+    }
+}
+</script>
 
+<style lang="scss" scoped>
 .ms_container-md {
     position: absolute;
     top: 0;
@@ -63,51 +56,13 @@
     left: 0;
 }
 
-
-
 .box-list {
     background-color: #FAFAFA;
     width: 522px;
 
-    .list-items {
-        display: flex;
-
+    h2 {
         padding: 30px 44px;
-
-        .sct-title {
-            font-size: 28px;
-        }
+        font-size: 28px;
     }
-}
-
-.lc-data {
-    width: 20%;
-    height: 55px;
-    background-color: $orange;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    color: #fff;
-}
-
-.lc-contents {
-    width: 80%;
-    padding: 0 20px;
-
-    .lc-info {
-        color: $gray;
-    }
-
-    .lc-read {
-        font-size: 20px;
-    }
-}
-
-.lc-title {
-    font-size: 20px;
-    font-family: "Libre Baskerville", serif;
-    font-weight: 700;
-    font-style: normal;
 }
 </style>
