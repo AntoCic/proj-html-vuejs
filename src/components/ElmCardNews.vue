80700b6c45eef7b5ade17d396a7b0c2697776c84
AntoCic <anto.cic.127@gmail.com>
2024-04-24 17:21:24 +0200

    FIXED section news

---
+++ src/components/ElmCardNews.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="card-news">
+    <div class="card-img position-relative">
+      <div class="badge-img text-gray fw-lighter"><i class="bi bi-tag"></i> Business, Leading</div>
+      <img :src="`/src/assets/img/${imgT}`">
+      <img :src="`/src/assets/img/${imgB}`">
+    </div>
+    <div class="card-content mt-4">
+      <p class="img-ref text-gray my-2"><i class="bi bi-clock"></i> {{ data }} <i class="bi bi-person"></i>{{ author }}
+      </p>
+      <h2 class="sct-title no-dot fs-3">{{ title }}</h2>
+      <p class="sct-sub-title">{{ content }}</p>
+      <a href="#" class="read-more"></a>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  props: ['img-t', 'img-b', 'data', 'author', 'title', 'content'],
+}
+</script>
+
+<style lang="scss" scoped>
+@use '../assets/scss/partials/_variables.scss' as *;
+
+.card-img {
+  img {
+    width: 100%;
+    aspect-ratio: 3/2;
+    object-fit: cover;
+    object-position: bottom;
+
+    &:last-child {
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      height: 60%;
+      width: 100%;
+    }
+  }
+
+  .badge-img {
+    position: absolute;
+    bottom: -10px;
+    right: 20px;
+    background-color: $orange;
+    color: #fff;
+    padding: 10px 20px;
+    z-index: 1;
+
+  }
+}
+.card-content {
+    i {
+      color: $orange;
+    }
+  }
+</style>
--- src/components/SctNews.vue
+++ src/components/SctNews.vue
@@ -1,47 +1,54 @@
 <template>
     <section class="news w-100 p-sct">
-        <div class="ms_container-md text-center">
 
-            <h2 class="sct-title">Latest news</h2>
-            <p class="sct-sub-title sct-sub-line-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic,
-                perferendis.</p>
-        </div>
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    <div class="ms_card p-2">
-                        <div class="card-img position-relative">
-                            <img src="../assets/img/h1-blog-img-02.jpg" alt="">
-                            <img src="../assets/img/h1-blog-img-03.jpg" alt="">
+        <div class="ms_container-lg position-relative">
+            <ElmArrow class="arr-left position-absolute top-50 start-0 translate-middle-y" />
+            <ElmArrow class="arr-right position-absolute top-50 end-0 translate-middle-y" />
+            <img src="../assets/img/svg/svg-4.svg" class="bg-decoration">
+            <div class="ms_container-md text-center">
+
+                <h2 class="sct-title">Latest news</h2>
+                <p class="sct-sub-title sct-sub-line-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Hic, perferendis.</p>
+                <div class="container mt-5">
+                    <div class="row text-start">
+                        <div class="col">
+                            <ElmCardNews img-t="h1-blog-img-02.jpg" img-b="h1-blog-img-03.jpg" data="May 5,2019"
+                                author="Amanda Doe" title="Next investment"
+                                content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eius mas tellus dolor ..." />
                         </div>
-                        <div class="card-content mt-3">
-                            <p class="img-ref text-gray my-2">May 5,2019 Amanda Doe</p>
-                            <h2 class="sct-title no-dot fs-3">Next investment</h2>
-                            <p class="sct-sub-title">Lorem ipsum dolor sit amet, consectetur
-                                adipisicing elit. Hic,
-                                perferendis.</p>
-                            <p>READ MORE</p>
+                        <div class="col">
+                            <ElmCardNews img-t="h1-blog-img-03.jpg" img-b="h1-img-01.jpg" data="May 5,2019"
+                                author="Amanda Doe" title="Team Building"
+                                content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eius mas tellus dolor ..." />
+                        </div>
+                        <div class="col">
+                            <ElmCardNews img-t="h1-img-01.jpg" img-b="h1-blog-img-01.jpg" data="May 5,2019"
+                                author="Amanda Doe" title="New Business"
+                                content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eius mas tellus dolor ..." />
                         </div>
                     </div>
                 </div>
-                <div class="col"></div>
-                <div class="col"></div>
             </div>
         </div>
+
     </section>
 
 </template>
 
-<style lang="scss" scoped>
-.card-img {
-    img:last-child {
-        position: absolute;
-        bottom: 0;
-        left: 0;
-        height: 60%;
-        width: 100%;
-        object-fit: cover;
-        object-position: bottom;
-    }
+<script>
+import ElmCardNews from './ElmCardNews.vue'
+import ElmArrow from './ElmArrow.vue';
+export default {
+    components: { ElmCardNews, ElmArrow },
 }
+</script>
+
+<style lang="scss" scoped>
+
+.bg-decoration{
+        position: absolute;
+        left: 0;
+        top: -200px;
+    }
 </style>
